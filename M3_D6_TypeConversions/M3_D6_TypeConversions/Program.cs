7f82b61b1b60d49b4ade8bc951a1c292ef8ede79
Kristina Reisener <kristina@yakha.eu>
2024-04-19 20:25:50 +0200

    M3 D6 Exercises uploaded

---
+++ M3_D6_TypeConversions/M3_D6_TypeConversions/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace M3_D6_TypeCasting
+{
+    internal class TypeCasting
+    {
+
+        //static double numD = 9.24; 
+        //static int numI = (int)numD; Explicit type conversion cannot be done outside of a method -> Exception if the variable is static
+        public void ImplicitCasting()
+        {
+            int num = 9;
+            double numD = num; //implicit type conversion
+            if (num == numD) //enters the if block because the bit code assigned to the numbers is still the same.
+            {
+                Console.WriteLine("inside if block");
+            }
+            Console.WriteLine(num);
+            Console.WriteLine(numD);
+        }
+
+        public void ExplicitCasting()
+        {
+            double numD = 9.99; //you cannot implicitly convert a bigger data type to a smaller data type
+            int numI = (int)numD;
+            Console.WriteLine(numI);
+            Console.WriteLine(numD);
+
+            //Careful of data loss
+
+        }
+
+        public void Parsing() //you cannot convert a double into an int using parsing, the exact format has to be used!
+        {
+            string numS = "10.5";
+            bool success = int.TryParse(numS, out int numI);
+
+            Console.WriteLine(success);
+            //int numI = int.Parse(numS);
+            Console.WriteLine(numI);
+            double numD = double.Parse(numS);
+            Console.WriteLine(numD);
+        }
+
+        public void ConvertClass()
+        {
+            string numI = "10";
+            //int numI = Convert.ToInt32(numS);
+            double numD = 10.25;
+            bool myBool = true;
+            string boolS  = "true";
+            Console.WriteLine(Convert.ToString(numI));
+            Console.WriteLine(Convert.ToDouble(numI));
+            Console.WriteLine(Convert.ToInt32(numD)); //data loss
+            Console.WriteLine(Convert.ToString(myBool));
+            Console.WriteLine(Convert.ToBoolean(boolS));    
+
+        }
+
+        class ArrayOperations
+        {
+            public void copyArray()
+            {
+                int[] myintegers = { 1, 2, 3, 4, 5 }; //declaration and definition
+                int[] myIntegers2 = new int[10]; // defining the array
+                myintegers.CopyTo(myIntegers2,2);
+                // 0 ->  index in the array in which the coyping begins
+                // -> the destination hast to be long  enough to contain the original array
+                // only the values on the assigned positions are overwritten, the rest remains the same
+                Array.Reverse(myIntegers2, 0, 10); //reverses the array
+                Array.Sort(myIntegers2); //sorts the array
+                foreach (int i in myIntegers2) {
+                    Console.Write(i);
+                }
+
+                Console.WriteLine(Array.IndexOf(myintegers, 4)); //gives you the first encountered value, then terminates
+
+            }
+        }
+        
+        internal class Methods
+        {
+
+            //you cannot do method overloading based on the return type (void, int, etc.)
+            public void add(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            public double add (double a, double b)
+            {
+                return a + b;
+            }
+
+            public double add (double a, double b, double c)
+            {
+                return a+b+c;
+            }
+
+            public void myMethod (string name = "Mathilde") //default value, when no parameter is 
+            {
+                Console.WriteLine (name);
+            }
+        }
+
+        internal class Recursion
+        {
+            //4!= $*3*2*1
+            public int factorial(int n)
+            {
+                if (n > 1)
+                    return n * factorial(n - 1);
+                else return n;
+               
+            }
+
+            public int factorialWithoutRecursion (int n)
+            {
+                /*
+                int i = n-1;
+                while (i > 1)
+                {
+                    n *= i;
+                    i--;
+                }
+                return n;
+                */
+                int i = n;
+                do
+                {
+                    
+                    if (n == 1) return n;
+                    else
+                    {
+                        n *= --i;
+                    } 
+                } while (i > 1);
+                return n;
+            }
+
+            
+            
+        }
+
+
+        public static void Main(string[] args)
+        {
+            // TypeCasting obj = new TypeCasting();
+            //obj.ConvertClass();
+            /*
+            ArrayOperations obj = new ArrayOperations();
+            obj.copyArray();
+            Methods m = new Methods();
+            m.myMethod();
+            m.myMethod("Anna");*/
+
+            Recursion obj = new Recursion();
+            Console.WriteLine(obj.factorial(4));
+            Console.WriteLine(obj.factorialWithoutRecursion(4));
+        }
+    }
+}
---
+++ M3_D6_WindowsExercises/M3_D6_WindowsExercises/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Globalization;
+//CultureInfo.CurrentCulture = new CultureInfo("en-US"); add after any other using statements
+
+
+namespace M3_D6_MicrosoftLearingExercises
+{
+    
+
+    class Datatypes
+    { 
+        public static void SignedIntegralTypes()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Signed integral types:");
+            Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+            Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+        }
+        public static void UnsignedIntegralTypes()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Unsigned integral types:");
+            Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+            Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+            Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+            Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+        }
+
+        public static void FloatingPointTypes()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Floating point types:");
+            Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+            Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+        }
+
+        public static void ReferenceTypes()
+        {
+            //When ref_B = ref_A is executed, ref_B points to the same memory location as ref_A.
+            //So, when ref_B[0] is changed, ref_A[0] also changes. 
+            int[] ref_A = new int[1];
+            ref_A[0] = 2;
+            int[] ref_B = ref_A;
+            ref_B[0] = 5;
+
+            Console.WriteLine("--Reference Types--");
+            Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+            Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+        }
+    }
+
+    class DataTypeConversion
+    {
+        public void dataConversion()
+        {
+
+            //widening conversion:
+            //converting a value from a data type that could hold less information to
+            //a data type that can hold more information. 
+            /*
+            int myInt = 3;
+            Console.WriteLine($"int: {myInt}");
+
+            decimal myDecimal = myInt;
+            Console.WriteLine($"decimal: {myDecimal}");*/
+
+            //Performing a cast /Explicit conversion
+            //Data loss possible
+            //narrowing conversion: Convert value from a data type that holds more information
+            //to a data type that holds less information
+            /*
+            decimal myDecimal = 3.14m;
+            Console.WriteLine($"decimal: {myDecimal}");
+
+            int myInt = (int)myDecimal;
+            Console.WriteLine($"int: {myInt}");*/
+
+            //Data Conversion with ToString()
+
+            /*
+            int first = 5;
+            int second = 7;
+            string message = first.ToString() + second.ToString();
+            Console.WriteLine(message); */
+
+            // Data Conversion with Parse()
+            /*
+            string first = "5";
+            string second = "7";
+            int sum = int.Parse(first) + int.Parse(second);
+            Console.WriteLine(sum);*/
+
+            //Data Conversion with Convert
+
+            /*
+            string value1 = "5";
+            string value2 = "7";
+            int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+            Console.WriteLine(result);*/
+            /*
+            int value = (int)1.5m; // casting truncates
+            Console.WriteLine(value);
+
+            int value2 = Convert.ToInt32(1.5m); // converting rounds up
+            Console.WriteLine(value2);*/
+
+            /* Try Parse() */
+
+            string value = "hi";
+            int result = 2;
+            if (int.TryParse(value, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+            if (result > 0)
+                Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+        }
+    }
+    
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            /******** data type conversion*****/
+           // DataTypeConversion data = new DataTypeConversion();
+            //data.dataConversion();
+
+            /**************datatypes********/
+            /**
+            Datatypes obj = new Datatypes();
+            Datatypes.UnsignedIntegralTypes();
+            Datatypes.SignedIntegralTypes();
+            Datatypes.FloatingPointTypes();
+            Datatypes.ReferenceTypes();
+            **/
+
+
+        }
+        
+    } 
+}
