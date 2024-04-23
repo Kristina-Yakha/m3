41e3a7d05018355e17cce2b3d33679b0d6780d07
Kristina Reisener <kristina@yakha.eu>
2024-04-23 08:19:03 +0200

    HighSchool Sweethearts uploaded

---
+++ M3_D7_HighSchoolSweethearts/M3_D7_HighSchoolSweethearts/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+
+public static class HighSchoolSweethearts
+{
+    public static string DisplaySingleLine(string studentA, string studentB)
+    {
+        return $"{studentA,29} ♡ {studentB,-29}";
+    }
+
+    private static string getInitials(string name)
+    {
+        return $"{name.Substring(0, 1)}. {name.Substring(name.IndexOf(" ") + 1, 1)}.";
+    }
+
+
+    public static string DisplayBanner(string studentA, string studentB)
+    {
+        studentA = getInitials(studentA);
+        studentB = getInitials(studentB);
+
+        string heart = "     ******       ******\n" +
+                       "   **      **   **      **\n" +
+                       " **         ** **         **\n" +
+                       "**            *            **\n" +
+                       "**                         **\n" +
+                       "**     {0}  +  {1}     **\n" +
+                       " **                       **\n" +
+                       "   **                   **\n" +
+                       "     **               **\n" +
+                       "       **           **\n" +
+                       "         **       **\n" +
+                       "           **   **\n " +
+                       "            ***\n" +
+                       "              *";
+
+        return String.Format(heart, studentA, studentB);
+
+    }
+
+    public static string DisplayGermanExchangeStudents(string studentA
+        , string studentB, DateTime start, float hours)
+    {
+
+        CultureInfo de = CultureInfo.CreateSpecificCulture("de-DE"); //start.ToString(de)
+        DateTimeFormatInfo dtfi = de.DateTimeFormat;
+        string startTime = start.ToString("d", dtfi);
+        string strHours = hours.ToString("N2", de);
+
+
+        return $"{studentA} and {studentB} have been dating since {startTime} - that's {strHours} hours";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
