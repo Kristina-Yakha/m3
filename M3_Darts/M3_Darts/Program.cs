a4aa3656f4c5f7f515b134829d814d3794712546
Kristina Reisener <kristina@yakha.eu>
2024-04-24 20:23:04 +0200

    M3_Darts uploaded

---
+++ M3_Darts/M3_Darts/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double coordinate = Math.Sqrt(x * x + y * y);
+
+        if (coordinate <= 1) return 10;
+        else if (coordinate <= 5) return 5;
+        else if (coordinate <= 10) return 1;
+        else return 0;
+
+    }
+}
