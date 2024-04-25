1dbb2373a5335006e65789fe0df6146c2149f6db
Kristina Reisener <kristina@yakha.eu>
2024-04-25 12:25:30 +0200

    M3_ResistorColor added

---
+++ M3_ResistorColor/M3_ResistorColor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        color = color.ToLower();
+        int colorCode = Array.IndexOf(Colors(), color);
+        return colorCode;
+    }
+
+    public static string[] Colors()
+    {
+        string[] colorArray = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+        return colorArray;
+
+    }
+
+}
