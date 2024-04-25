55175936ad2da9216c3ad0d95b8f82064091ac4b
Kristina Reisener <kristina@yakha.eu>
2024-04-25 12:25:01 +0200

    M3_Proverb added

---
+++ M3_Proverb/M3_Proverb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string[] verses = new string[subjects.Length];
+
+        for (int i = 0; i < subjects.Length; i++)
+        {
+            if (i == subjects.Length - 1) verses[verses.Length - 1] = $"And all for the want of a {subjects[0]}.";
+            else verses[i] = $"For want of a {subjects[i]} the {subjects[i + 1]} was lost.";
+        }
+        return verses;
+    }
+}
