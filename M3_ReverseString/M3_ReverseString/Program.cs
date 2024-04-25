6469c3fc03ded951e9fc349aa537c6b550c19c68
Kristina Reisener <kristina@yakha.eu>
2024-04-25 16:31:38 +0200

    M3_ReverseString added

---
+++ M3_ReverseString/M3_ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] inputString = input.ToCharArray();
+        string reverseString = "";
+
+        for (int i = inputString.Length - 1; i >= 0; i--)
+        {
+            reverseString += inputString[i].ToString();
+        }
+        return reverseString;
+
+    }
+}
