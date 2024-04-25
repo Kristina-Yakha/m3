dcbb14be24f1d32e7d7fc64ca4b5b66eda37aa48
Kristina Reisener <kristina@yakha.eu>
2024-04-25 12:27:56 +0200

    M3_RollTheDie added

---
+++ M3_RollTheDie/M3_RollTheDie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Player
+{
+    public int RollDie()
+    {
+        Random dice = new Random();
+        return dice.Next(1, 19);
+
+    }
+
+    public double GenerateSpellStrength()
+    {
+        Random spell = new Random();
+        return spell.NextDouble() * 100;
+    }
+}
+
