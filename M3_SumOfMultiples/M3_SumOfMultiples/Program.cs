d931868d83a38afb891d913a0ce8845fa963a182
Kristina Reisener <kristina@yakha.eu>
2024-04-25 12:28:32 +0200

    M3_SumOfMultiples added

---
+++ M3_SumOfMultiples/M3_SumOfMultiples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public static class SumOfMultiples
+{
+
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+
+        List<int> uniqueMultiples = new List<int>();
+        //the number that is to be
+        int newListItem = 0;
+        int sum = 0;
+
+
+        foreach (int multiple in multiples)
+        {
+
+            int factor = 0;
+            if (multiple != 0) factor = (int)((max - 1) / multiple);
+
+            while (factor > 0)
+            {
+
+                //calculate multiple
+                newListItem = multiple * factor;
+
+                //if number does not exist in the List, add number
+                if (!uniqueMultiples.Contains(newListItem)) uniqueMultiples.Add(newListItem);
+
+                factor--;
+
+            }
+        }
+
+        //iterate through the list and sum the multiples
+
+        foreach (int uniqueMultiple in uniqueMultiples)
+        {
+            sum += uniqueMultiple;
+        }
+        return sum;
+    }
+}
+
