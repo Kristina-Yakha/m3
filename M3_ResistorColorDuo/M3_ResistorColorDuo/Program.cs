c6bb8a06c77b337b31adbed2f0a32177b625394b
Kristina Reisener <kristina@yakha.eu>
2024-04-25 12:27:04 +0200

    M3_ResistorColorDuo added

---
+++ M3_ResistorColorDuo/M3_ResistorColorDuo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class ResistorColorDuo
+{
+
+
+    public static int Value(string[] colors)
+    {
+        string[] colorCodes = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+        //finde die erste farbe aus colors in dem colorArray und gib den index zurück
+
+        string digit = "";
+
+
+        for (int i = 0; i <= 1; i++)
+        {
+            for (int j = 0; j < colorCodes.Length; j++)
+            {
+                if (colors[i] == colorCodes[j])
+                {
+                    digit += j.ToString();
+                }
+            }
+
+        }
+
+
+
+        return Int32.Parse(digit);
+    }
+}
