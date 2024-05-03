777ce19c09622486010be396251178aa9ccbd873
Kristina Reisener <kristina@yakha.eu>
2024-05-03 16:23:13 +0200

    M3_BookingForBeauty added

---
+++ M3_BookingForBeauty/M3_BookingForBeauty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        return DateTime.Parse(appointmentDateDescription);
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        DateTime currentDateTime = DateTime.Now;
+
+        if (appointmentDate < currentDateTime) return true;
+        else return false;
+
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+
+        int hour = appointmentDate.Hour;
+        if (hour >= 12 && hour < 18) return true;
+        else return false;
+        //>= 12:00 and < 18:00
+
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+
+        return $"You have an appointment on {appointmentDate:G}.";
+
+
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+
+        return new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
+
+
+    }
+}
