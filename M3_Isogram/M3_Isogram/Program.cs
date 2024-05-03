e23c7095bd8b49a4ba0eaa83341ef685ef740eb2
Kristina Reisener <kristina@yakha.eu>
2024-05-03 16:24:38 +0200

    M3_Isogram added

---
+++ M3_Isogram/M3_Isogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        word = word.ToLower();
+        Char[] letters = word.ToCharArray();
+
+        bool isIsogram = true;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+
+            if (Char.IsWhiteSpace(letters[i])) continue;
+            else if (letters[i] == '-') continue;
+            else
+            {
+                for (int j = 0; j<letters.Length; j++)
+                {
+                    if (letters[j] == letters[i] && j != i) isIsogram = false;
+                 
+                }
+                
+            }
+            
+        }
+        return isIsogram;
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsIsogram("isogram"));
+    }
+}
