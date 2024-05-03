981e4c99a4f494912d1f579f35f23b058ef335e3
Kristina Reisener <kristina@yakha.eu>
2024-05-03 16:25:06 +0200

    M3_Raindrops added

---
+++ M3_Raindrops/M3_Raindrops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string drippingSound = "";
+        if (number % 3 == 0)
+        {
+            drippingSound += "Pling";
+        }
+        if (number % 5 == 0)
+        {
+            drippingSound += "Plang";
+        }
+        if (number % 7 == 0)
+        {
+            drippingSound += "Plong";
+        }
+
+        else if ((number % 3 != 0) && (number % 5 != 0) && (number % 7 != 0))
+        {
+            drippingSound = number.ToString();
+        }
+        return drippingSound;
+    }
+}
