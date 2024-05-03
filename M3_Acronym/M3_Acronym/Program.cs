74d7ec83dd04271187dd27d34714465fb8b87d00
Kristina Reisener <kristina@yakha.eu>
2024-05-03 20:27:57 +0200

    M3_Acronym added

---
+++ M3_Acronym/M3_Acronym/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        phrase = Regex.Replace(phrase, @"\s*-+\s*|\s*_+\s*", " ");
+        string[] splitPhrase = phrase.Split(' ');
+        string acronym = "";
+
+        foreach (string word in splitPhrase)
+        {
+            acronym += word.Substring(0, 1);
+        }
+        return acronym.ToUpper();
+    }
+}
