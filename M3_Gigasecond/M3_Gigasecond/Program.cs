703b54b52fce09ec8d12f37b544a7c098ffa54ee
Kristina Reisener <kristina@yakha.eu>
2024-05-07 19:03:48 +0200

    M3_Gigasecond added

---
+++ M3_Gigasecond/M3_Gigasecond/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Gigasecond
+{
+    public static DateTime Add(DateTime moment)
+    {
+        double gigaSecond = 1000000000.00;
+        return moment.AddSeconds(gigaSecond);
+
+    }
+}
