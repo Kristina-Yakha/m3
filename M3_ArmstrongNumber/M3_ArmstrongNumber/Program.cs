ff0f6e6a89acc4a30e3261281082bcde3bccd1bf
Kristina Reisener <kristina@yakha.eu>
2024-05-07 19:40:52 +0200

    M3_ArmstrongNumber added

---
+++ M3_ArmstrongNumber/M3_ArmstrongNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        char[] digits = Convert.ToString(number).ToCharArray();
+        double numberOfDigits = digits.Length;
+        double sum = 0;
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            sum += Math.Pow(Convert.ToInt32(new string(digits[i], 1)), numberOfDigits);
+        }
+
+
+        if (sum == number)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
